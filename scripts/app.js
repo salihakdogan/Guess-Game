b6a3b4c30df4ba217a13cf92661f91a83013d4b9
salihakdogan <salihakdogan1@gmail.com>
2023-04-13 21:32:08 +0300

    the index.html and main.css files were edited. the first part of the game logic was coded in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,11 @@
-//When the user clicks on button approve terms and conditions, close the modal
-$(".accept").on("click", function() {
+//If the user approves the informative text, the modal will be closed. A true-valued object will be created for the accepted key in local storage.
+$("#modalButton").on("click", function() {
 	$("#modal").css("display","none");
 	playsound("clickAccept");
 	localStorage.setItem("accepted", "true");
+	setTimeout( function name() {
+		window.location.reload();
+	},2000);
 });
 
 //Closes the modal belonging to the information message when clicked anywhere on the screen
@@ -10,13 +13,140 @@ $(window).on("click", function() {
 	$("#modal").css("display","none");
 })
 
-//Opens the modal when the page is loaded if accepted key is not allowed
+//If the value of the accepted key is not true, the informative modal will be opened again on page load and in the mouseover event of screen1Square.
 const isItAccepted = localStorage.getItem("accepted");
 if (!isItAccepted) {
 	$(window).on("load", function () {
 		$("#modal").css("display","block");
 	});
+	$("#screen1Square").mouseover(function () {
+		$("#modal").css("display","block");
+	});
+} else { //If the accepted key is true, the second screen will be transitioned to in the mouseover event of screen1Square.
+	$("#screen1Square").mouseover(function () {
+		$(this).animate({
+			width: "16rem",
+			height: "16rem"
+		}, 300);
+		setTimeout(function () {
+			$("#screen1").addClass("hide");
+			$("#screen2").addClass("show");
+			$("#screen2 .left").addClass("opacity");
+		}, 700);
+	});
 }
+
+$(".hover").hover(function () {
+	$(this).toggleClass("border");
+});
+
+
+var started = false;
+var score = 0;
+
+$(".middle").click(function () {
+	started = true;
+	$("#screen2").removeClass("show")
+	$("#screen2").addClass("hide");
+	$("#screen3").addClass("show");
+	$("#screen3 .right").addClass("opacity");
+
+});
+
+const randomNumber1 = Math.floor((Math.random() * 100));
+$("#randomNumber1").html(randomNumber1);
+
+
+$("#up").click(function() {
+	calculate("up");
+});
+
+$("#down").click(function() {
+	calculate("down");
+});
+
+function calculate(params) {
+	const randomNumber2 = Math.floor((Math.random() * 100));
+	$("#randomNumber2").html(randomNumber2);
+
+	$("#up").addClass("hide");
+	$("#down").addClass("hide");
+
+	if (randomNumber1 > randomNumber2) {
+		$("#symbol").html(">");
+		if (params === "up") {
+			$("#result").html("lose");
+			startOver();
+
+		} else {
+			$("#result").html("win");
+			$("#up").toggleClass("hide");
+			$("#down").toggleClass("hide");
+			score++;
+		}
+
+	} else {
+		$("#symbol").html("<");
+		if (params === "up") {
+			$("#result").html("win");	
+			$("#up").toggleClass("hide");
+			$("#down").toggleClass("hide");
+			score++;
+
+		} else {
+			$("#result").html("lose");
+			startOver();
+		}
+	}
+	$("#score").html(score);
+}
+
+function startOver() {
+	score = 0;
+	started = false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //A function that takes audio files as parameters and plays
 function playsound(sound) {
