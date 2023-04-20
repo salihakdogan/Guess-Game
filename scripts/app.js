25571e4e3e521c5cb3931d905edceffb27ec7e82
salihakdogan <salihakdogan1@gmail.com>
2023-04-20 23:55:18 +0300

    the settings modal and its functions were added, and visual errors were fixed

--- scripts/app.js
+++ scripts/app.js
@@ -13,7 +13,7 @@ $("#modalButton").on("click", function () {
 
 //Closes the modal belonging to the information message when clicked anywhere on the screen
 $(".modal").on("click", function () {
-	$("#informationModal").css("display","none");
+	$("#informationModal").css("display", "none");
 });
 
 //If the value of the accepted key is not true, the informative modal will be opened again on page load and in the mouseover event of screen1Square.
@@ -43,20 +43,25 @@ if (!isItAccepted) {
 	});
 }
 
-$("#settingsButton").on("click", function() {
-	$("#settingsModal").css("display","flex");
+$("#settingsButton").on("click", function () {
+	$("#settingsModal").css("display", "flex");
 });
 
-$("#closeButton").click(function() {
-	$("#settingsModal").css("display","none");
+$("#closeButton").click(function () {
+	$("#settingsModal").css("display", "none");
+	setTimeout(function () {
+		window.location.reload();	
+	}, 500);
 });
 
 /* game starting here */
 var started = false;
 var score = 0;
 
-function setHighScore(scorePoint) {
-	playsound("setHighScore");
+function setHighScore(scorePoint, operation) {
+	if (operation !== "deleteStats") {
+		playsound("setHighScore");
+	}
 	localStorage.setItem("highScore", scorePoint);
 }
 
@@ -87,7 +92,11 @@ var randomNumber2;
 
 function startGame() {
 	$("#result").html("");
-	$("#symbol").html("and");
+	if (languageSetting === "eng") {
+		$("#symbol").html("and");
+	} else {
+		$("#symbol").html("ve");	
+	}
 	randomNumber1 = Math.floor((Math.random() * 100));
 	$("#randomNumber1").html(randomNumber1);
 	randomNumber2 = "??";
@@ -183,8 +192,10 @@ function animateElement(elementId, className) {
 
 //A function that takes audio files as parameters and plays
 function playsound(sound) {
-	var audio = new Audio("/sources/sounds/" + sound + ".mp3");
-	audio.play();
+	if (soundSetting === "on") {
+		var audio = new Audio("/sources/sounds/" + sound + ".mp3");
+		audio.play();
+	}
 }
 
 //A function that deletes a local storage object captured by a parameter for testing purposes
@@ -211,3 +222,96 @@ function setProgressBar(duration) {
 		}
 	}, 10);
 }
+
+$("#soundOn").on("click", function () {
+	$(this).addClass("active");
+	$("#soundOff").removeClass("active");
+	localStorage.setItem("sound", "on");
+});
+
+$("#soundOff").on("click", function () {
+	$(this).addClass("active");
+	$("#soundOn").removeClass("active");
+	localStorage.setItem("sound", "off");
+});
+
+$("#themeDark").on("click", function () {
+	$(this).addClass("active");
+	$("#themeLight").removeClass("active");
+	localStorage.setItem("theme", "dark");
+});
+
+$("#themeLight").on("click", function () {
+	$(this).addClass("active");
+	$("#themeDark").removeClass("active");
+	localStorage.setItem("theme", "light");
+});
+
+$("#languageTr").on("click", function () {
+	$(this).addClass("active");
+	$("#languageEng").removeClass("active");
+	localStorage.setItem("language", "tr");
+});
+
+$("#languageEng").on("click", function () {
+	$(this).addClass("active");
+	$("#languageTr").removeClass("active");
+	localStorage.setItem("language", "eng");
+});
+
+const soundSetting = localStorage.getItem("sound");
+const themeSetting = localStorage.getItem("theme");
+const languageSetting = localStorage.getItem("language");
+
+$(window).on("load", function () {
+	if (soundSetting === "on") {
+		$("#soundOn").toggleClass("active");
+	} else {
+		$("#soundOff").toggleClass("active");
+	}
+
+	if (themeSetting === "dark") {
+		$("#themeDark").toggleClass("active");
+	} else {
+		$("#themeLight").toggleClass("active");
+	}
+
+	if (languageSetting === "tr") {
+		$("#languageTr").toggleClass("active");
+		$(".tr").toggleClass("hide");
+		$(".eng").toggleClass("hide");
+
+	} else {
+		$("#languageEng").toggleClass("active");
+	}
+
+	if (getHighScore() === "") {
+		$("#deleteStatRow").toggleClass("hide");
+	}
+
+	if (themeSetting === "dark") {
+		$(":root").css({
+			"--primary":"#161616",
+			"--secondary":"#d9d9d9"
+		});
+	}
+});
+
+$("#deleteStats").on("click", function () {
+	$("#deleteYes").toggleClass("hide");
+	$("#deleteNo").toggleClass("hide");
+});
+
+$("#deleteNo").on("click", function () {
+	$("#deleteYes").toggleClass("hide");
+	$("#deleteNo").toggleClass("hide");
+});
+
+$("#deleteYes").on("click", function () {
+	setHighScore("", "deleteStats");
+	setTimeout(function () {
+		window.location.reload();
+	}, 1000)
+	$("#deleteYes").toggleClass("hide");
+	$("#deleteNo").toggleClass("hide");
+});
